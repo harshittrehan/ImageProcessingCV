81dc92e6fcf56368234c13a92bb624446d8256c0
Harshit Trehan <hegi.trehan@gmail.com>
2020-12-12 02:57:14 -0800

    Readme added

---
+++ ImageProcessingCV/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ImageProcessingCV
+//
+//  Created by Harshit Trehan on 12/12/20.
+//  Copyright © 2020 Harshit Trehan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
